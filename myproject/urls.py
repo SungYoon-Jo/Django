d1bec17564ab3ede548484c907a72bad97670461
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-24 14:32:02 +0900

    add migrate

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('onememos', include('onememos.urls')),
     path('admin/', admin.site.urls),
+    path('onememos', include('onememos.urls')),
 ]
