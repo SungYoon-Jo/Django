102d89b5410a356304d8a27af5f9ecd164fc91bb
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-24 13:20:54 +0900

    마이그레이션 세팅

--- onememos/models.py
+++ onememos/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Memo(models.Model):
+    memo_text = models.CharField(max_length=200)
+    published_date = models.DateTimeField(auto_now_add=True)
