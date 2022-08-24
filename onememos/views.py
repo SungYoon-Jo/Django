93697c5eddc19692ef4cc5534a973227f449a76e
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-24 15:01:10 +0900

    add get

--- onememos/views.py
+++ onememos/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def index(request):
     return HttpResponse("Onememos~ Hello, World~ ")
+
+def createMemo(request):
+
+    request.GET['somekey']
+
+    return HttpResponse('')
