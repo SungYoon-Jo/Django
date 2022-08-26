4c4f09c3ad3b93647163670f470539843a7c22f4
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-26 13:03:39 +0900

    add delete, modify moudle

--- onememos/urls.py
+++ onememos/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.main, name='main'),
     path('createMemo/', views.createMemo),
     path('writeMemo/', views.writeMemo),
+    path('modify/<str:idx>', views.modifyPage),
+    path('modify/update/', views.updateMemo),
+    path('delete/<str:idx>', views.deleteMemo),
 ]
--- onememos/views.py
+++ onememos/views.py
@@ -6,28 +6,46 @@ from .models import *
 
 
 def main(request):
-    # return render(request, 'main.html')
-    
     lists = Memo.objects.all()
     data = {'lists' : lists}
     
     return render(request, 'main.html', data)
 
-
 def createMemo(request):
-    # memoContent = request.POST['memoContent']
+    memoContent = request.POST['memoContent']
     
-    # article = Memo(memo_text = memoContent)
-    # article.save()
+    article = Memo(memo_text = memoContent)
+    article.save()
     
-    # return HttpResponse('create memo = ' + memoContent)
-
     return HttpResponseRedirect(reverse('main'))
 
 def writeMemo(request):
-    # return HttpResponse('writeMemo good')
-    
     if request.method == 'GET':
         return HttpResponse("to GET")
     if request.method == 'POST':
         return HttpResponse('to POST')
+
+def modifyPage(request, idx):
+    article = Memo.objects.get(id=idx)
+    data = {'article': article}
+    
+    return render(request, 'modify.html', data)
+
+def updateMemo(request):
+    idx = request.POST['idx']
+    memoContent = request.POST['memoContent']
+    
+    db_article = Memo.objects.get(id = idx)
+    db_article.memo_text = memoContent
+    db_article.save()
+    
+    return HttpResponseRedirect(reverse('main'))
+    
+def deleteMemo(request, idx):
+
+    db_article = Memo.objects.get(id=idx)    
+    db_article.delete()
+    
+    return HttpResponseRedirect(reverse('main'))
+    return HttpResponseRedirect(reverse('main'))
+    return HttpResponseRedirect(reverse('main'))
