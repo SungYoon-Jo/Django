116ca6879672f6188e90704073f52c515fd2ed87
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-26 14:35:19 +0900

    del dir

--- django_project/board/admin.py
+++
@@ -1,9 +0,0 @@
-import imp
-from django.contrib import admin
-from board.models import Board
-
-class BoardAdmin(admin.ModelAdmin):
-    list_display = ('writer', 'title', 'content')
-
-admin.site.register(Board, BoardAdmin)
-
