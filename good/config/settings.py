2c92f81149cd426f97238d2a84f3096bca6c1376
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-27 00:01:09 +0900

    connect mysql

--- good/config/settings.py
+++ good/config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'board',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'PASSWORD': '1q2w3e',
+        'USER': 'root',
     }
 }
 
