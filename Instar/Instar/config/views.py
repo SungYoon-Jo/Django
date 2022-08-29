26ef30dde8085ae2ca46c70e6065960f1a4f1a47
Jo  Seongyun <whtjddbs0112@naver.com>
2022-08-29 14:25:33 +0900

    new django

---
+++ Instar/Instar/config/views.py
@@ -0,0 +1,11 @@
+import imp
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Sub(APIView):
+    def get(self, request):
+        print('get')
+        return render(request,"instar/main.html")
+    def psot(self, request):
+        print('post')
+        return render(request, "instar/main.html")
