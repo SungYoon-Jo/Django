cacd626151e56ab7201b38cf8a081834ba82687b
Jo  Seongyun <whtjddbs0112@naver.com>
2022-09-02 01:35:15 +0900

    회원가입 및 로그인 프론트 구현

---
+++ Instar/Instar/content/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import UploadFeed
+
+
+
+urlpatterns = [
+    path('upload', UploadFeed.as_view())
+]
+
+
+
+
---
+++ Instar/Instar/user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import Join, Login
+
+
+urlpatterns = [
+    path('join', Join.as_view()),
+    path('login', Login.as_view()),
+
+]
+
+
